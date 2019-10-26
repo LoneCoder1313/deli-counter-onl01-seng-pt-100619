ab23222c960b2a5c88a5e2bc2be06bbff3d8a997
Ricky dildy <rdildy92@gmail.com>
2019-10-26 03:57:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,10 +6,8 @@ end
 def take_a_number(katz_deli) do 
   katz_deli.collect_with_index |line|
   puts take_a_number(katz_deli, #{name})
-  # Welcome, Ada. You are number 1 in line.
-  # Welcome, Grace. You are number 2 in line.
-  #3 Welcome, Kent. You are number 3 in line.
-  #binding.pry
 end 
 end 
+
+def now_serving()
   
