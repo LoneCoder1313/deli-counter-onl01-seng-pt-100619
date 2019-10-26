1cfe38d878a3002372e294829b21cf9d6d67bf18
Ricky dildy <rdildy92@gmail.com>
2019-10-26 03:51:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,5 +10,7 @@ def take_a_number(katz_deli) do
   # Welcome, Ada. You are number 1 in line.
   # Welcome, Grace. You are number 2 in line.
   #3 Welcome, Kent. You are number 3 in line.
-  
+  binding.pry 
+end 
+end 
   
